7cf3587a894a2bd04581e56359c28340ef1e147a
stelour <stelourgg@gmail.com>
2025-04-12 00:22:31 +0500

    created register and login forms with working db on postgres

--- app/models.py
+++ app/models.py
@@ -1,15 +1,45 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from app import login
 
 
-class User(db.Model):
-  id: so.Mapped[int] = so.mapped_column(primary_key=True)
-  username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-  email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-  password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    
+    profile: so.Mapped["Profile"] = so.relationship(back_populates="user")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Profile(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), unique=True)
+    full_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
+    bio: so.Mapped[Optional[str]] = so.mapped_column(sa.Text)
+    avatar_url: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    birth_date: so.Mapped[Optional[datetime]] = so.mapped_column(sa.DateTime)
+    location: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
+
+    user: so.Mapped["User"] = so.relationship(back_populates="profile")
   
-  def __repr__(self):
-    return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<Profile of {}>'.format(self.user.username)
 
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-  SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1@localhost/noxchat'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1@localhost/noxchat'
