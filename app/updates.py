678d1e9fd4e34f2f1e7f13b3e31c529e93ae874e
stelour <stelourgg@gmail.com>
2025-07-13 19:42:29 +0500

    added some styles

--- app/updates.py
+++ app/updates.py
@@ -5,23 +5,28 @@ updates = [
         "description": "Initial release with basic features, profile editing, and the ability to add friends."
     },
     {
-        "version": "v0.2",
+        "version": "v0.1.1",
         "date": "2025-06-21",
         "description": "Added the ability to search for users on the site, view friends and common requests. Also fixed some bugs."
     },
     {
-        "version": "v0.3",
+        "version": "v0.1.2",
         "date": "2025-07-11",
         "description": "Email support and reset password."
     },
     {
-        "version": "v0.3.1",
+        "version": "v0.2",
         "date": "2025-07-11",
         "description": "Added style for login and register form."
     },
     {
-        "version": "v0.3.2",
-        "date": "2025-07-11",
+        "version": "v0.2.1",
+        "date": "2025-07-12",
         "description": "Added Logo for NoxChat and added some styles."
+    },
+    {
+        "version": "v0.2.2",
+        "date": "2025-07-13",
+        "description": "Added some styles and fixed some bugs."
     }
 ]
